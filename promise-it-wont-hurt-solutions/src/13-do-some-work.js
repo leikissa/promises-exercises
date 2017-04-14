2394aa092fdce5669f90ce788339cd0ddd90d969
Sally Maki <sallymaki@gmail.com>
2017-04-14 12:20:14 -0700

    Finished exercise 13

--- promise-it-wont-hurt-solutions/src/13-do-some-work.js
+++ promise-it-wont-hurt-solutions/src/13-do-some-work.js
@@ -1,1 +1,9 @@
 'use strict';
+var http = require("q-io/http");
+
+http.read('http://localhost:7000')
+.then(function(id){
+  return http.read('http://localhost:7001/' + id)
+})
+.then(JSON.parse)
+.then(console.log)
