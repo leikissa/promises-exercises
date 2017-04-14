8bfd8262bd2ea5e75852b078d7fdc0b53d3f084c
Sally Maki <sallymaki@gmail.com>
2017-04-14 11:08:05 -0700

    Finished exercises 11 and 12

--- promise-it-wont-hurt-solutions/src/11-multiple-promises.js
+++ promise-it-wont-hurt-solutions/src/11-multiple-promises.js
@@ -1,17 +1,24 @@
 'use strict';
 
 function all(promise1, promise2){
-  var counter = 0;
-  var promise = new Promise(function(fulfill, reject){
-    if (counter === 2) {
-      fulfill([])
-    }
-    fulfill(counter++)
+  return new Promise(function(fulfill, reject){
+    var counter = 0;
+    var result = []
+    promise1.then(function(input){
+      result[0] = input
+      counter++
+      if (counter === 2) {
+        fulfill(result)
+      }
+    })
+    promise2.then(function(input){
+      result[1] = input
+      counter++
+      if (counter === 2) {
+        fulfill(result)
+      }
+    })
   })
-  promise1.then()
-  promise2.then()
-  counter++
-  return promise
 }
 
 all(getPromise1(),getPromise2())
--- promise-it-wont-hurt-solutions/src/12-fetch-json.js
+++ promise-it-wont-hurt-solutions/src/12-fetch-json.js
@@ -1,1 +1,8 @@
 'use strict';
+var http = require("q-io/http");
+
+var promise = http.read('http://localhost:1337')
+
+promise
+.then(JSON.parse)
+.then(console.log)
